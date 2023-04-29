94a395d1c69561f9911e9de3cee252d375b27450
YUVRAJ SINGH <yyuvrajssingh2001@gmail.com>
2023-04-29 19:56:19 +0530

    third

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -27,43 +27,47 @@ const deleteJob = async (req, res) => {
   await job.deleteOne();
   res.status(StatusCodes.OK).json({ msg: 'Success! job removed' });
 };
-const getAllJobs = async (req, res) => {
-  // const { status, jobType, sort, search } = req.query;
-  // const queryObject = {
-  //   createdBy: req.user.userId,
-  // };
+//get all jobs
 
-  // if (status !== 'all') {
-  //   queryObject.status = status;
-  // }
-  // if (jobType && jobType !== 'all') {
-  //   queryObject.jobType = jobType;
-  // }
-  // if (search) {
-  //   queryObject.position = { $regex: search, $options: 'i' };
-  // }
-  // let result = Job.find(queryObject);
-  // if (sort === 'latest') {
-  //   result = result.sort('-createdAt');
-  // }
-  // if (sort === 'oldest') {
-  //   result = result.sort('createdAt');
-  // }
-  // if (sort === 'a-z') {
-  //   result = result.sort('position');
-  // }
-  // if (sort === 'z-a') {
-  //   result = result.sort('-position');
-  // }
-  // const page = Number(req.query.page) || 1;
-  // const limit = Number(req.query.limit) || 10;
-  // const skip = (page - 1) * limit;
-  // result = result.skip(skip).limit(limit);
-  // const jobs = await result;
-  // const totalJobs = await Job.countDocuments(queryObject);
-  // const numOfPages = Math.ceil(totalJobs / limit);
-  const jobs = await Job.find({});
-  res.status(StatusCodes.OK).json({ jobs });
+const getAllJobs = async (req, res) => {
+  const { status, jobType, sort, search } = req.query
+
+  const queryObject = {
+      createdBy:req.user.userId
+  }
+
+  if ( status !== 'all') {
+      queryObject.status=status
+  }
+  if (jobType && jobType !== 'all') {
+      queryObject.jobType=jobType
+  }
+  if (search) {
+  queryObject.position={$regex:search,$options:'i'}
+  }
+  let result = Job.find(queryObject)
+  if (sort === 'latest') {
+      result=result.sort('-createdAt')
+  }
+  if (sort === 'oldest') {
+      result=result.sort('createdAt')
+  }
+  if (sort === 'a-z') {
+      result=result.sort('position')
+  }
+  if (sort === 'z-a') {
+      result=result.sort('-position')
+  }
+  const page = Number(req.query.page)||1
+  const limit = Number(req.query.limit)||10;
+  const skip = (page-1)*limit
+  result=result.skip(skip).limit(limit)
+  const jobs=await result
+  const totalJobs=await Job.countDocuments(queryObject)
+  const numOfPages=Math.ceil(totalJobs/limit)
+  res.
+      status(StatusCodes.OK).
+      json({ jobs, totalJobs, numOfPages })
 };
 
 const updateJob = async (req, res) => {
