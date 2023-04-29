2782a29c0618f58acc305fab52e6de67efdc28a7
Ankit Jha <jhaa11701@gmail.com>
2023-04-30 01:38:20 +0530

    added login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
-import Dashboard from './pages/Dashboard'
+import Dashboard from './pages/Dashboard';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/register' element={<Register />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        
-      </Routes>
+      <div className='h-screen w-screen flex items-center'>
+        <Routes>
+          <Route path='/register' element={<Register />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 export default App;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,21 +1,45 @@
 const Register = () => {
-    return (
-        <div className="h-[70%] w-[50%] bg-[#B2BCFE] rounded-md absolute top-[15%] left-[20%]">
-            <div className="ml-10 mt-10">
-                <p className="font-bold text-2xl">Login</p>
-                <p className="mt-3 text-[#64748B] ">Welcome back,Enter your credentials to access your account</p>
-                <p className="text-[20px] mt-2">Email Address</p>
-                <input className="rounded-md w-[90%] h-[40px] mt-2" type="text" placeholder="Enter your email" />
-                <p className="text-[20px]  mt-2 ">Password</p>
-                <input type="text" className="border-none rounded-md w-[90%] h-[40px]" placeholder="Enter your password" />
-                <div className="mt-[40px] h-[44px]">
-                <button className="bg-[#00083B] h-full rounded-md  w-[90%] text-white">Continue</button>                
-                </div>
-                <div>
-                <p className=" ml-[20%] mt-4 ">Do you have an account?<span className="text-[#00083B]  font-bold">Sign up Here</span></p>                
-                </div>
-            </div>
+  return (
+    <div className='h-[70%] w-[50%] bg-[#B2BCFE] rounded-xl shadow-xl mx-auto '>
+      <div className='ml-10 mt-10'>
+        <p className='font-bold text-2xl'>Login</p>
+        <p className='mt-3 text-[#64748B] '>
+          Welcome back,Enter your credentials to access your account
+        </p>
+        <p className='text-[20px] mt-2'>Name</p>
+
+        <input
+          className='rounded-md w-[90%] h-[40px] mt-2 outline-none px-2'
+          type='text'
+          placeholder='Enter your name'
+        />
+
+        <p className='text-[20px] mt-2'>Email Address</p>
+
+        <input
+          className='rounded-md w-[90%] h-[40px] mt-2 outline-none px-2'
+          type='text'
+          placeholder='Enter your email'
+        />
+        <p className='text-[20px]  mt-2 '>Password</p>
+        <input
+          type='password'
+          className='border-none rounded-md w-[90%] h-[40px] outline-none px-2'
+          placeholder='Enter your password'
+        />
+        <div className='mt-5 h-[44px]'>
+          <button className='bg-[#00083B] h-full rounded-md  w-[90%] text-white'>
+            Continue
+          </button>
         </div>
-    )
-}
-export default Register
+        <div>
+          <p className=' ml-[20%] mt-4 '>
+            Do you have an account?
+            <span className='text-[#00083B]  font-bold'> Sign up Here</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Register;
