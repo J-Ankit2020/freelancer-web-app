7ad82c93b6ee5ce257d1faa1f60c16649d8b8124
Ankit Jha <jhaa11701@gmail.com>
2023-04-29 17:57:11 +0530

    added some fixes

--- server/app.js
+++ server/app.js
@@ -1,11 +1,19 @@
-import express from 'express';
 import { config } from 'dotenv';
 config();
+import express from 'express';
+import { connectMongoose } from './db/mongoose.js';
 
 const app = express();
 
+// connect to MongoDB
+connectMongoose();
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.listen(5000, () => {
+  console.log('Server listening on port 3000');
 });
 
 export default app;
--- server/server.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js';
-import { connectMongoose } from './db/mongoose.js';
-
-connectMongoose();
-
-app.listen(5000, () => {
-  console.log('Server listening on port 3000');
-});
