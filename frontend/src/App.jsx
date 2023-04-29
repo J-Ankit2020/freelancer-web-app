65fdaacbbfc51cebbfb624b0c5faf5d8fdfd5976
Ankit Jha <jhaa11701@gmail.com>
2023-04-29 22:09:26 +0530

    added tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
-export default function App() {
-  return (
-    <div className="bg-red-300">
-      <p className="font-bold">font</p>
-    </div>
-  )
+import './index.css';
+
+function App() {
+  return <h1 className='text-blue-700'>Hello world!</h1>;
 }
+
+export default App;
